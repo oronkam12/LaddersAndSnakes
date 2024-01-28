70db7276cd169de2fabfead0439ca53a0e6d2842
Nadav <nadavcarmi1@gmail.com>
2024-01-28 21:46:30 +0200

    Added comments to Ladder and Snake objects

--- src/Ladder.java
+++ src/Ladder.java
@@ -31,26 +31,35 @@ public class Ladder extends Object implements ObjectInterface{
 	public void draw(Graphics2D g, int cellSize) {
 	    Cell headCell = this.getHeadCell();
 
+	    //Set dimensions in pixels - start
 	    int startX = (headCell.getCol() + 1) * cellSize;
 	    int startY = (headCell.getRow() + 1) * cellSize;
 
 	    int endX = (this.getButtomCell().getCol() + 1) * cellSize;
 	    int endY = (this.getButtomCell().getRow() + 1) * cellSize;
-
+	    //Set dimensions in pixels - end
+	    
+	    //Calculate angle to rotate image
 	    double angleInRadians = Math.atan2(endY - startY, endX - startX) + (105* Math.PI / 180.0) - (10* Math.PI / 180.0) ;
+	    
+	 	//Calculate the middle of the image
 	    int midX = (startX + endX) / 2;
 	    int midY = (startY + endY) / 2;
-
+	    
+	    //Loading and scaling the image to fit board (Some of the calculations are for design implementations)
 	    BufferedImage snakeImage = this.getImage();
 	    Image resizedSnakeImage = snakeImage.getScaledInstance(cellSize/(Math.abs(headCell.getCol()-buttomCell.getCol())+1) * (Math.abs(headCell.getCol() - this.getButtomCell().getCol()) + 2),  cellSize*2 * (Math.abs(headCell.getRow() - this.getButtomCell().getRow())), Image.SCALE_SMOOTH);
 
+	    //Calculating middle location of scaled image
 	    int drawX = midX - resizedSnakeImage.getWidth(null) / 2;
 	    int drawY = midY - resizedSnakeImage.getHeight(null) / 2;
 
+	    //Transforming the scaled image in rotation and scales
 	    AffineTransform transform = new AffineTransform();
 	    transform.translate(drawX - cellSize / 2, drawY - cellSize / 2);
 	    transform.rotate(angleInRadians, resizedSnakeImage.getWidth(null) / 2.0, resizedSnakeImage.getHeight(null) / 2.0);
 
+	    //Drawing in the GUI
 	    g.drawImage(resizedSnakeImage, transform, null);  // Fix here
 	}
 
--- src/Snake.java
+++ src/Snake.java
@@ -13,27 +13,35 @@ public abstract class Snake extends Object implements ObjectInterface {
 	public void draw(Graphics2D g, int cellSize) {
 	    Cell headCell = this.getHeadCell();
 
-	    int startX = (headCell.getCol() + 1) * cellSize;
+	    //Set dimensions in pixels - start
+	    int startX = (headCell.getCol() + 1) * cellSize;	
 	    int startY = (headCell.getRow() + 1) * cellSize;
 
 	    int endX = (this.getButtomCell().getCol() + 1) * cellSize;
 	    int endY = (this.getButtomCell().getRow() + 1) * cellSize;
-
+	    //Set dimensions in pixels - end
+	    
+	    //Calculate angle to rotate image
 	    double angleInRadians = Math.atan2(endY - startY, endX - startX);
 
+	    //Calculate the middle of the image
 	    int midX = (startX + endX) / 2;
 	    int midY = (startY + endY) / 2;
 
+	    //Loading and scaling the image to fit board (Some of the calculations are for design implementations)
 	    BufferedImage snakeImage = this.getImage();
 	    Image resizedSnakeImage = snakeImage.getScaledInstance(cellSize * (Math.abs(headCell.getCol() - this.getButtomCell().getCol()) + 2), cellSize * (Math.abs(headCell.getRow() - this.getButtomCell().getRow())), Image.SCALE_SMOOTH);
-
+	    
+	    //Calculating middle location of scaled image
 	    int drawX = midX - resizedSnakeImage.getWidth(null) / 2;
 	    int drawY = midY - resizedSnakeImage.getHeight(null) / 2;
 
+	    //Transforming the scaled image in rotation and scales
 	    AffineTransform transform = new AffineTransform();
 	    transform.translate(drawX - cellSize / 2, drawY - cellSize / 2);
 	    transform.rotate(angleInRadians, resizedSnakeImage.getWidth(null) / 2.0, resizedSnakeImage.getHeight(null) / 2.0);
 
+	    //Drawing in the GUI
 	    g.drawImage(resizedSnakeImage, transform, null);  // Fix here
 	}
 
