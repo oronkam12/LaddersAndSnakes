eac50a8dfef34e3199695664b3cd478253264739
oronkam12 <oronkam12@gmail.com>
2024-01-28 22:59:52 +0200

    fixed 2 packages

--- src/Viewers/ObjectInterface.java
+++
@@ -1,9 +0,0 @@
-package Viewers;
-import java.awt.Graphics2D;
-
-import Model.Player;
-
-public interface ObjectInterface {
-	void MovePlayer(Player player);
-	void draw(Graphics2D g, int cellSize);
-}
